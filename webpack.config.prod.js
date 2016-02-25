c2cccb84f762860bd65e969e341aa88686a03e29
Swizec Teller <swizec@swizec.com>
2016-02-25 01:34:31 -0800

    Disable hmr?

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -18,11 +18,11 @@ module.exports = {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
+   // new webpack.optimize.UglifyJsPlugin({
+   //   compressor: {
+   //     warnings: true
+   //   }
+   // })
   ],
   resolve: {
     extensions: ['', '.js', '.jsx']
