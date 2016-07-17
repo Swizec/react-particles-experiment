c1d6661daa0140a6ccbda560561758a5cce4d3f6
Swizec Teller <swizec@swizec.com>
2016-07-17 15:48:29 -0700

    Dropping one layer below Konva is still too slow :(

--- src/components/Particles/index.jsx
+++ src/components/Particles/index.jsx
@@ -24,59 +24,35 @@ class Particles extends Component {
 
     _particles = {};
 
+    componentDidMount() {
+        this.canvas = this.refs.layer.canvas._canvas;
+        this.context = this.canvas.getContext('2d');
+    }
+
+    drawParticle(particle) {
+        let { x, y } = particle;
+
+        this.context.beginPath();
+        this.context.arc(x, y, 1, 0, 2*Math.PI, false);
+        this.context.lineWidth = 1;
+        this.context.strokeStyle = 'black';
+        this.context.stroke();
+    }
+
     componentDidUpdate() {
-        let layer = this.refs.the_thing,
-            particles = this.props.particles,
+        let particles = this.props.particles,
             removedParticles = this.props.removedParticles;
 
-        // add or move particles
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         for (let i = 0; i < particles.length; i++) {
-            let { id, x, y } = particles[i];
-
-            if (this._particles[id]) {
-                // move particle
-                this._particles[id]
-                    .position({
-                        x: x,
-                        y: y
-                    });
-            }else{
-                // create new particle
-                let c = new Circle({
-                    radius: 1.4,
-                    x: x,
-                    y: y,
-                    fill: 'black',
-                    transformsEnabled: 'position',
-                    listening: false
-                });
-                this._particles[id] = c;
-                layer.add(c);
-            }
-        };
-
-        // remove removed particles
-        for (let i = 0; i < removedParticles.length; i++) {
-            let { id } = removedParticles[i];
-
-            if (this._particles[id]) {
-                this._particles[id].remove();
-                delete this._particles[id];
-            }
+            this.drawParticle(particles[i]);
         }
-
-        layer.batchDraw();
-
-        //console.timeEnd('flag1');
-
-        /* {particles.map(particle =>
-           <Circle radius="1.8" x={particle.x} y={particle.y} key={particle.id} fill="black" />
-           )} */
     }
 
     render() {
         return (
-            <FastLayer ref="the_thing" transformsEnabled="position" listening="false">
+            <FastLayer ref="layer" transformsEnabled="position" listening="false">
 
             </FastLayer>
         );
