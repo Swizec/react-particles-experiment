cd409d1861f4c0a1ba828f7e47680fe71620eecd
Swizec Teller <swizec@swizec.com>
2017-05-07 17:54:46 -0700

    Sync code with book

--- src/components/Particles/index.jsx
+++ src/components/Particles/index.jsx
@@ -1,15 +1,11 @@
 
 import React, { Component } from 'react';
-import { findDOMNode } from 'react-dom';
 
 import Particle from './Particle';
 
 const Particles = ({ particles }) => (
     <g>
-        {particles.map(particle =>
-            <circle style={{transform: `translate3d(${particle.x}px, ${particle.y}px, 0px) scale(1)`}}
-            r="1.8"
-            key={particle.id} />)}
+        {particles.map(particle => <Particle {...particle} key={particle.id} />)}
     </g>
 )
 
